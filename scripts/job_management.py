0deb2fbe52920b3b511371ad997a980835187f32
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2019-12-03 16:14:30 +0100

    add unix pattern matching to shift restriction option

--- scripts/job_management.py
+++ scripts/job_management.py
@@ -207,7 +207,13 @@ def prepare_jobs(input_ntuples_list, inputs_base_folder, inputs_friends_folders,
             if len(restrict_to_channels_file) > 0 or (len(restrict_to_channels_file) == 0 and len(restrict_to_channels) > 0):
                 pipelines = [p for p in pipelines if p.split("_")[0] in restrict_to_channels_file]
             if len(restrict_to_shifts) > 0:
-                pipelines = [p for p in pipelines if p.split("_")[1] in restrict_to_shifts]
+                pipelines_restricted = []
+                for p in pipelines:
+                    for shift in restrict_to_shifts:
+                        if fnmatch.fnmatch(p.split("_")[1], shift):
+                            pipelines_restricted.append(p)
+                            break
+                pipelines = pipelines_restricted
             ntuple_database[nick]["pipelines"] = {}
             for p in pipelines:
                 print "Pipeline: {}".format(p)
